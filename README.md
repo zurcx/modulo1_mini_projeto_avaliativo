b950e46d7c02f619b1e38c5c370de2f5b1bcf872
Luiz Fábio da Cruz <luizfabiodacruz@gmail.com>
2026-05-30 21:38:13 -0300

    feat: Finalizando o GroupBy

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# Problemas encontradoes
+
+# Problemas encontrados
 ## Espaços no inicio da Categoria (PR_CAT)
 ## Espaços no inicio da Nome do Produto (PR_NOME)
 ## Colunas nao nomeadas
 ## Colunas com valores nulos
+
+
+# Etapas de limpeza
+## Removida colunas unnamed e com dados nulos
+## Conversão do tipo relacionada a data.
+## Identificado datas nulas, porém a opção foi a verificação em outras fontes que possam dar mais evidencia para tomada de decisão
+## Encontrado uma categoria '#N/D' foi classificada com 'Outros'
+
+
--- main.py
+++ main.py
@@ -41,7 +41,8 @@ print(df.head(10))
 # Exibição das 10 ultimas linhas
 print("\n\n" + " As 10 ultimas linhas do DataSet".center(80, "=") + "\n")
 print(df.tail(10))
-
+print()
+"""
 #  Mostrar o tamanho (linhas e colunas)
 
 print("\n" + " Tamanho do Dataset ".center(80, "=") + "\n")
@@ -62,7 +63,7 @@ print(df.columns)
 print()
 print("\n" + " Fim bloco Colunas ".center(80, "="))
 print()
-
+"""
 Evidencias(df, "Base de Loja Varejo")
 
 print("\n" + " TRATAMENTO DOS DADOS ".center(80, "=") + "\n")
@@ -71,7 +72,7 @@ print("\n" + " TRATAMENTO DOS DADOS ".center(80, "=") + "\n")
 
 df = df.dropna(axis=1, how="all")
 
-print(df.columns)
+# print(df.columns)
 
 
 # Padronização de produto
@@ -88,5 +89,108 @@ print("\n" + f"Validação padronização coluna PR_CAT:\n \n{df['PR_CAT']}")
 # Tratar falso nulos
 
 df = tratar_falso_nulos(df)
+df.loc[df["PR_CAT"] == "#N/D", "PR_CAT"] = "Outros"
+
+# Tratar tipos de dados
+
 df["DATA"] = pd.to_datetime(df["DATA"], errors="coerce")
-Evidencias(df, "Base de Loja Varejo")
+
+qtd_nulos_datas = df["DATA"].isna().sum()
+perc_nulos = qtd_nulos_datas / len(df) * 100
+
+print(f"percentual de datas nulas: {perc_nulos:.2f}%")
+
+# Devido ao percentual de nulos vamos separar
+
+print("\n" + " Dados df com datas preenchidas".center(80, "=") + "\n")
+df_com_data = df[df["DATA"].notna()]
+print(f"Linhas:     {df_com_data.shape[0]}")
+print(f"Colunas:    {df_com_data.shape[1]}")
+print()
+print(df_com_data.head(10))
+
+print("\n" + " Dados df sem datas preenchidas".center(80, "=") + "\n")
+df_sem_data = df[df["DATA"].isna()]
+print(f"Linhas:     {df_sem_data.shape[0]}")
+print(f"Colunas:    {df_sem_data.shape[1]}")
+print()
+print(df_sem_data.head(10))
+
+"""
+# cliente 814
+
+df_814 = df[df["CL_ID"] == 814]
+
+print("cliente 814")
+print(df_814.head())
+print(df_814.shape)
+df.groupby("CO_ID")["DATA"].nunique().sort_values(ascending=False).head(10)
+df[df["DATA"].isna()]["CL_ID"].value_counts().head(10)
+df[df["DATA"].isna()]["CL_ID"].value_counts().head(10)
+print(df[df["CO_ID"] == 8878]["DATA"])
+"""
+
+qtd_duplicados_data = df.duplicated(subset=["DATA", "CL_ID", "PR_NOME"]).sum()
+
+# tratar qtd_duplicados
+
+print("\n" + " Quantidade de registros duplicados ".center(80, "=") + "\n")
+
+duplicados = df[df.duplicated(subset=["DATA", "CO_ID", "CL_ID"], keep=False)]
+
+print(duplicados.head())
+
+print("\n" + " Amostra de duplicados ".center(80, "=") + "\n")
+print(duplicados["CO_ID"].value_counts())
+print()
+print(duplicados.head(10))
+
+# tratar Duplicatas
+
+df_limpo = df.drop_duplicates(subset=["DATA", "CO_ID", "CL_ID"], keep="first")
+
+Evidencias(df_limpo, "Base de Loja Varejo")
+
+
+# estatistica numero de filhos
+
+print("\n" + " Estatística sobre filhos dos clientes".center(80, "=") + "\n")
+
+estat_filhos = df_limpo["CL_FHL"].describe().round(2)
+print(estat_filhos)
+
+
+# Agrupamento da analise
+
+
+print("\n" + " Agrupamento por categoria ".center(80, "=") + "\n")
+resumo_categ = (
+    df_limpo.groupby("PR_CAT")
+    .agg(
+        total=("PR_ID", "sum"),
+        media=("PR_ID", "mean"),
+        quantidade=("PR_ID", "count"),
+        maximo=("PR_ID", "max"),
+    )
+    .sort_values("total", ascending=False)
+)
+
+print(resumo_categ)
+
+print("\n" + " Agrupamento por categoria ".center(80, "=") + "\n")
+resumo_filhos_categ = (
+    df_limpo.groupby("PR_CAT")
+    .agg(media_filhos=("CL_FHL", "mean"), clientes=("CL_ID", "nunique"))
+    .round(2)
+)
+
+print(resumo_filhos_categ)
+
+
+print("\n" + " Resumo da análise".center(80, "=") + "\n")
+print("* ")
+print("* ")
+print("* ")
+print("* ")
+print("* ")
+print("* ")
