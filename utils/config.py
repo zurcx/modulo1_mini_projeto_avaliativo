800f03a0f9fe1b253272357928763519cf48aa2e
Luiz Fábio da Cruz <luizfabiodacruz@gmail.com>
2026-05-31 17:46:20 -0300

    feat: Geração dos graficos

--- utils/config.py
+++ utils/config.py
@@ -9,3 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # configuração dos diretorios
 DATA_DIR = BASE_DIR / "data"
 OUTPUT_DIR = BASE_DIR / "outputs"
+NOTEBOOKS_DIR = BASE_DIR / "notebooks"
+
+OUTPUT_DIR.mkdir(exist_ok=True)
